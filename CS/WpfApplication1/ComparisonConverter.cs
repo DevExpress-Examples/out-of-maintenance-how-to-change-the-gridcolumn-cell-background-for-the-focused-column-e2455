120e12dcc8c01eca666e94148bb18589c9ce4799
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/ComparisonConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WpfApplication1 {
+    public class ComparisonConverter : MarkupExtension, IMultiValueConverter {
+        public ComparisonConverter() { }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return ReferenceEquals(values[0], values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
